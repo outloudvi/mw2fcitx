713b6bd462453cee5854f2f53cbafef58ebdd9d0
Outvi V <git@outv.im>
2024-07-22 00:07:04 +0800

    feat: yapf and tests

--- mw2fcitx/build_dict.py
+++ mw2fcitx/build_dict.py
@@ -20,8 +20,7 @@ def build(config):
         if isinstance(title_file_path, str):
             title_file_path = [title_file_path]
         for i in title_file_path:
-            pipeline.load_titles_from_file(i,
-                                           **config["source"].get("kwargs"))
+            pipeline.load_titles_from_file(i, **config["source"].get("kwargs"))
     pipeline.convert_to_words(config["tweaks"])
     pipeline.export_words(config["converter"].get("use"),
                           **config["converter"].get("kwargs"))
