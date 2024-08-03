155eb099183fbf358e50afafc9ea47db131b9c72
Outvi V <git@outv.im>
2024-08-04 00:19:07 +0800

    chore: use pytest-cov in place of coverage

--- utils/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-poetry install
-poetry run coverage erase
-poetry run coverage run -m pytest tests/
-poetry run coverage run -a -m mw2fcitx.main -c tests/cli/conf_one.py
-poetry run coverage html
