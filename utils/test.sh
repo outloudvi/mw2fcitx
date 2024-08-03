67477913f0ec9653b75cc6bfc380a86188c6572a
Outvi V <git@outv.im>
2024-08-04 00:00:52 +0800

    feat: setup coverage and codecov

--- utils/test.sh
+++ utils/test.sh
@@ -3,6 +3,6 @@ set -e
 
 poetry install
 poetry run coverage erase
-poetry run coverage run -m pytest
+poetry run coverage run -m pytest tests/
 poetry run coverage run -a -m mw2fcitx.main -c tests/cli/conf_one.py
 poetry run coverage html
