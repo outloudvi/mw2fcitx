ae465e2cfad976b7be8b3b14ec7d8910aeef33a9
Outvi V <git@outv.im>
2024-08-23 22:31:44 +0800

    tests: various tests

--- tests/cli/test_conf_one.py
+++
@@ -1,9 +0,0 @@
-from mw2fcitx.main import inner_main
-
-
-def test_inner_main_name_without_py():
-    inner_main(['-c', 'tests/cli/conf_one'])
-
-
-def test_inner_main_name_with_py():
-    inner_main(['-c', 'tests/cli/conf_one.py'])
