623b8b4b19f431eaef08d0844b277f26b73f6395
Outvi V <git@outv.im>
2024-08-28 22:31:42 +0800

    tests: test on `continue` parsing, `aplimit` and `request_delay`

--- tests/cli/test_conf.py
+++ tests/cli/test_conf.py
@@ -1,3 +1,4 @@
+import time
 from mw2fcitx.main import inner_main
 import pytest
 
@@ -24,6 +25,14 @@ sort: by_weight
 ...
 初音未来	chu yin wei lai
 """)
+
+
+def test_continue():
+    # this should run at least 8 secs = 2 * (5 / 1) - 2
+    start = time.perf_counter()
+    inner_main(['-c', 'tests/cli/conf_continue'])
+    end = time.perf_counter()
+    assert (end-start > 8)
 
 
 def test_err_no_path():
