2807f18a3a66afd06fd339d91b536ab5b5f6f9ba
Outvi V <git@outv.im>
2024-12-23 21:17:23 +0800

    feat: disable_instinct_pinyin

--- tests/lib/test_exporters_opencc.py
+++ tests/lib/test_exporters_opencc.py
@@ -19,5 +19,22 @@ def test_opencc_exporter():
             "测试",
             "无效:词条"
         ]) == "测试\tce'shi\t0\n"
+    )
 
+def test_opencc_instinct_pinyin(): # outloudvi/mw2fcitx#29
+    assert (
+        export([
+            "唔呣",
+            "嗯啊啊"
+        ]) == "唔呣\twu'mu\t0\n"
+        "嗯啊啊\ten'a'a\t0\n"
+    )
+
+    assert (
+        export([
+            "唔呣",
+            "嗯啊啊"
+        ], disable_instinct_pinyin = True
+        ) == "唔呣\twu'm\t0\n"
+        "嗯啊啊\tn'a'a\t0\n"
     )
