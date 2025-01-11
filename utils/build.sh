498072a7bd5c43abf5e5717519c0293a922f8ef4
Outvi V <git@outv.im>
2025-01-12 00:42:20 +0800

    chore: downgrade fakeroot
    see: docker/for-mac#7331

--- utils/build.sh
+++ utils/build.sh
@@ -13,6 +13,8 @@ useradd archbuild
 chmod -R a+rwx .
 export DATE=${1:-$(date +%Y%m%d)} # makepkg.sh also uses the variable
 sed -i "s/0.0.1/$DATE/" utils/moegirl_dict.py
+# https://github.com/docker/for-mac/issues/7331#issuecomment-2198313216
+sudo pacman -U --noconfirm https://archive.archlinux.org/packages/f/fakeroot/fakeroot-1.34-1-x86_64.pkg.tar.zst
 su archbuild utils/makepkg.sh
 
 cp fcitx5-pinyin-moegirl* /artifacts
