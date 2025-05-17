22b3a5c7df52a59d9f9eb0ea279a9cd8dec4fad3
Outvi V <git@outv.im>
2025-05-17 12:10:15 +0800

    feat(tweaks): add tweak_dedup

--- mw2fcitx/tweaks/moegirl.py
+++ mw2fcitx/tweaks/moegirl.py
@@ -102,6 +102,11 @@ def tweak_opencc_t2s(words):
     return ret
 
 
+def tweak_dedup(words):
+    from mw2fcitx.utils import dedup
+    return dedup(words)
+
+
 tweaks = [
     tweak_remove_word_includes(["○", "〇"]),
     tweak_split_word_with(
@@ -112,4 +117,5 @@ tweaks = [
     tweak_remove_regex(["^第.*(次|话)$"]),
     tweak_normalize,
     tweak_opencc_t2s,
+    tweak_dedup,
 ]
