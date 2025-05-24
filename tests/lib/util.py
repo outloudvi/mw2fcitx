4b226a5584d7a6bc2d9e9708b3f7f762e15814a4
Outvi V <git@outv.im>
2025-05-24 23:06:47 +0800

    chore: remove unused code

--- tests/lib/util.py
+++ tests/lib/util.py
@@ -7,8 +7,6 @@ REAL_WORLD_ENV_NAME = "TEST_REAL_WORLD"
 def requires_real_world():
     real_world_test_flag = os.environ.get(REAL_WORLD_ENV_NAME) or "false"
 
-    print(f"___________________{real_world_test_flag}")
-
     return pytest.mark.skipif(
         real_world_test_flag.lower() != "true",
         reason=f"Skipping real-world test as ${REAL_WORLD_ENV_NAME} is not `true`"
