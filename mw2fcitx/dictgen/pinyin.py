8052ddbb83b63894e46a1fa8520ef386801a6548
Outvi V <git@outv.im>
2025-06-11 22:24:34 +0800

    chore: use `logging.getLogger(__name__)`

--- mw2fcitx/dictgen/pinyin.py
+++ mw2fcitx/dictgen/pinyin.py
@@ -1,21 +1,23 @@
+import logging
 import tempfile
 import subprocess
 
 from ..const import LIBIME_BIN_NAME, LIBIME_REPOLOGY_URL
-from ..logger import console
+
+log = logging.getLogger(__name__)
 
 
 def gen(text, **kwargs):
     with tempfile.NamedTemporaryFile("w+") as file:
         file.write(text)
-        console.info(f"Running {LIBIME_BIN_NAME}...")
+        log.info(f"Running {LIBIME_BIN_NAME}...")
         try:
             subprocess.run([LIBIME_BIN_NAME, file.name, kwargs["output"]],
                            check=True)
         except FileNotFoundError:
-            console.error(
+            log.error(
                 f"The program \"{LIBIME_BIN_NAME}\" is not found. "
                 f"Please install libime: {LIBIME_REPOLOGY_URL}"
             )
             raise
-        console.info("Dictionary generated.")
+        log.info("Dictionary generated.")
--- mw2fcitx/fetch.py
+++ mw2fcitx/fetch.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 import json
 from os import access, R_OK
@@ -8,8 +9,9 @@ from .const import ADVANCED_MODE_TRIGGER_PARAMETER_NAMES, \
     PARTIAL_CONTINUE_DICT, \
     PARTIAL_DEPRECATED_APCONTINUE, \
     PARTIAL_TITLES
-from .logger import console
 from .utils import create_requests_session
+
+log = logging.getLogger(__name__)
 
 
 s = create_requests_session()
@@ -20,14 +22,14 @@ def save_to_partial(partial_path: str, titles: List[str], continue_dict: dict):
     try:
         with open(partial_path, "w", encoding="utf-8") as fp:
             fp.write(json.dumps(ret, ensure_ascii=False))
-        console.debug(f"Partial session saved to {partial_path}")
+        log.debug(f"Partial session saved to {partial_path}")
     except Exception as e:
-        console.error(str(e))
+        log.error(str(e))
 
 
 def resume_from_partial(partial_path: str) -> tuple[List[str], dict]:
     if not access(partial_path, R_OK):
-        console.warning(f"Cannot read partial session: {partial_path}")
+        log.warning(f"Cannot read partial session: {partial_path}")
         return [[], None]
     try:
         with open(partial_path, "r", encoding="utf-8") as fp:
@@ -42,8 +44,8 @@ def resume_from_partial(partial_path: str) -> tuple[List[str], dict]:
                 }
             return (titles, continue_dict)
     except Exception as e:
-        console.error(str(e))
-        console.error("Failed to parse partial session")
+        log.error(str(e))
+        log.error("Failed to parse partial session")
         return [[], None]
 
 
@@ -51,7 +53,7 @@ def warn_advanced_mode(custom_api_params: dict, triggerer_param_names: List[str]
     is_advanced_mode = False
     for param_name in triggerer_param_names:
         if param_name in custom_api_params:
-            console.warning(
+            log.warning(
                 f"I'm seeing `{param_name}` in `api_params`. "
                 "Advanced Mode is enabled. "
                 "No parameter except for `format` will be injected automatically."
@@ -69,7 +71,7 @@ def populate_api_params(custom_api_params: dict, deprecated_aplimit: Union[None,
     if not warn_advanced_mode(custom_api_params, ADVANCED_MODE_TRIGGER_PARAMETER_NAMES):
         # Deprecated `aplimit`
         if deprecated_aplimit is not None:
-            console.warning(
+            log.warning(
                 "Warn: `source.kwargs.aplimit` is deprecated - "
                 "please use `source.kwargs.api_param.aplimit` instead.")
         aplimit = int(deprecated_aplimit) \
@@ -92,24 +94,24 @@ def populate_api_params(custom_api_params: dict, deprecated_aplimit: Union[None,
 def fetch_all_titles(api_url: str, **kwargs) -> List[str]:
     title_limit = kwargs.get(
         "api_title_limit") or kwargs.get("title_limit") or -1
-    console.debug(f"Fetching titles from {api_url}" +
-                  (f" with a limit of {title_limit}" if title_limit != -1 else ""))
+    log.debug(f"Fetching titles from {api_url}" +
+              (f" with a limit of {title_limit}" if title_limit != -1 else ""))
     titles = []
     partial_path = kwargs.get("partial")
     time_wait = float(kwargs.get("request_delay", "2"))
     custom_api_params = kwargs.get("api_params", {})
     if not isinstance(custom_api_params, dict):
-        console.error(
+        log.error(
             f"Type of `api_params` is not dict or None, but {type(custom_api_params)}")
         sys.exit(1)
 
     api_params = populate_api_params(custom_api_params, kwargs.get("aplimit"))
     if partial_path is not None:
-        console.info(f"Partial session will be saved/read: {partial_path}")
+        log.info(f"Partial session will be saved/read: {partial_path}")
         [titles, continue_dict] = resume_from_partial(partial_path)
         if continue_dict is not None:
             api_params.update(continue_dict)
-            console.info(
+            log.info(
                 f"{len(titles)} titles found. Continuing from {continue_dict}")
     resp = s.get(api_url, params=api_params)
     initial_data = resp.json()
@@ -122,7 +124,7 @@ def fetch_all_titles(api_url: str, **kwargs) -> List[str]:
         partial_path,
         time_wait
     )
-    console.info("Finished.")
+    log.info("Finished.")
     return titles
 
 
@@ -142,15 +144,15 @@ def fetch_all_titles_inner(
         if "error" in data:
             error_code = data["error"].get("code", "?")
             error_msg = data["error"].get("info", str(data["error"]))
-            console.error(
+            log.error(
                 f"MediaWiki API error: [code: {error_code}] {error_msg}"
             )
         if not "query" in data:
-            console.error(
+            log.error(
                 "No `query` found in response. "
                 "Please check the response body for any potential issues:"
             )
-            console.error(data)
+            log.error(data)
             sys.exit(1)
 
         for (_, item_value) in data["query"].items():
@@ -158,20 +160,20 @@ def fetch_all_titles_inner(
         if title_limit != -1 and len(titles) >= title_limit:
             titles = titles[:title_limit]
             break
-        console.debug(f"Got {len(titles)} pages")
+        log.debug(f"Got {len(titles)} pages")
         if "continue" in data:
             time.sleep(time_wait)
             try:
                 continue_dict = data["continue"]
-                console.debug(f"Continuing from {continue_dict}")
+                log.debug(f"Continuing from {continue_dict}")
                 api_params = initial_api_params.copy()
                 api_params.update(continue_dict)
                 data = s.get(api_url, params=api_params).json()
             except Exception as e:
                 if isinstance(e, KeyboardInterrupt):
-                    console.error("Keyboard interrupt received. Stopping.")
+                    log.error("Keyboard interrupt received. Stopping.")
                 else:
-                    console.error(str(e))
+                    log.error(str(e))
                 if partial_path:
                     save_to_partial(partial_path, titles, continue_dict)
                 sys.exit(1)
